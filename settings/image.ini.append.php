c2c9fb559f18a4009b16409ecd513f25966da8a0
Xiongjie Chen <xc@ez.no>
2010-09-16 08:34:38 +0000

    Cleaned text
    git-svn-id: http://svn.projects.ez.no/epie/trunk@105 621ec555-2961-457b-befc-e0ae98695168

--- settings/image.ini.append.php
+++ settings/image.ini.append.php
@@ -3,9 +3,7 @@
 # This is here for development purposes only and example.
 # The settings must not be overriden in the extension.
 #
-# TODO: remove this file.
 [eZIE]
-#watermarks[]
 watermarks[]=elephpant.png
 watermarks[]=ez-logo.png
 
