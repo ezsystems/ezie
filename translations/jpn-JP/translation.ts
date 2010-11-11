348d673da3edd6c8825236b8d717dfa1a20189b2
Eric Sagnes <mail@eric-sagnes.fr>
2010-11-11 12:28:35 +0900

    Added Japanese ts file

---
+++ translations/jpn-JP/translation.ts
@@ -0,0 +1,186 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ja_JP">
+<context>
+    <name>design/standard/content/datatype</name>
+    <message>
+        <source>Current image</source>
+        <translation>現在のイメージ</translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation>プレビュー</translation>
+    </message>
+    <message>
+        <source>Filename</source>
+        <translation>ファイル名</translation>
+    </message>
+    <message>
+        <source>MIME type</source>
+        <translation>MIMEタイプ</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>サイズ</translation>
+    </message>
+    <message>
+        <source>There is no image file.</source>
+        <translation>画像ファイルがありません。</translation>
+    </message>
+    <message>
+        <source>Remove image</source>
+        <translation>イメージの削除</translation>
+    </message>
+    <message>
+        <source>New image file for upload</source>
+        <translation>アップロードする画像ファイル</translation>
+    </message>
+    <message>
+        <source>Alternative image text</source>
+        <translation>画像代替テキスト</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/ezie</name>
+    <message>
+        <source>Edit</source>
+        <translation>編集</translation>
+    </message>
+    <message>
+        <source>eZ Image Editor</source>
+        <translation>eZイメージエディター</translation>
+    </message>
+    <message>
+        <source>Save and Close</source>
+        <translation>保存して終了</translation>
+    </message>
+    <message>
+        <source>Save &amp;amp; Close</source>
+        <translation>保存して終了</translation>
+    </message>
+    <message>
+        <source>Close without saving</source>
+        <translation>保存せす終了</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>終了</translation>
+    </message>
+    <message>
+        <source>Thumbnail</source>
+        <translation>サムネイル</translation>
+    </message>
+    <message>
+        <source>Rotation</source>
+        <translation>回転</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>選択</translation>
+    </message>
+    <message>
+        <source>Dimensions</source>
+        <translation>寸法</translation>
+    </message>
+    <message>
+        <source>Keep ratio</source>
+        <translation>比率を維持</translation>
+    </message>
+    <message>
+        <source>Free</source>
+        <translation>自由</translation>
+    </message>
+    <message>
+        <source>Crop</source>
+        <translation>切り取り</translation>
+    </message>
+    <message>
+        <source>Zoom</source>
+        <translation>拡大</translation>
+    </message>
+    <message>
+        <source>Actual pixels</source>
+        <translation>実際のサイズ</translation>
+    </message>
+    <message>
+        <source>Fit on screen</source>
+        <translation>全体表示</translation>
+    </message>
+    <message>
+        <source>Watermarks</source>
+        <translation>透かし</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation>実行</translation>
+    </message>
+    <message>
+        <source>Contrast</source>
+        <translation>コントラスト</translation>
+    </message>
+    <message>
+        <source>Brightness</source>
+        <translation>明度</translation>
+    </message>
+    <message>
+        <source>Actions</source>
+        <translation>アクション</translation>
+    </message>
+    <message>
+        <source>Tools</source>
+        <translation>ツール</translation>
+    </message>
+    <message>
+        <source>Undo</source>
+        <translation>取り消し</translation>
+    </message>
+    <message>
+        <source>Redo</source>
+        <translation>やり直し</translation>
+    </message>
+    <message>
+        <source>Image</source>
+        <translation>画像</translation>
+    </message>
+    <message>
+        <source>Horizontal Flip</source>
+        <translation>左右反転</translation>
+    </message>
+    <message>
+        <source>Vertical Flip</source>
+        <translation>上下反転</translation>
+    </message>
+    <message>
+        <source>Perform Crop</source>
+        <translation>切り抜き実行</translation>
+    </message>
+    <message>
+        <source>Watermark</source>
+        <translation>透かし</translation>
+    </message>
+    <message>
+        <source>Effects</source>
+        <translation>エフェクト</translation>
+    </message>
+    <message>
+        <source>Pixelate</source>
+        <translation>モザイク</translation>
+    </message>
+    <message>
+        <source>Black and White</source>
+        <translation>白黒</translation>
+    </message>
+    <message>
+        <source>Sepia</source>
+        <translation>セピア</translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>オプション</translation>
+    </message>
+</context>
+</TS>
