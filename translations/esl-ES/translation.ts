a018e6d2c93b6020813b2cf708e3f05b6d3e02b2
crevillo <crevillo@gmail.com>
2011-03-03 03:31:24 +0800

    location: added esl-ES translation.ts

---
+++ translations/esl-ES/translation.ts
@@ -0,0 +1,194 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>design/standard/content/datatype</name>
+    <message>
+        <source>Current image</source>
+        <translation>Imagen actual</translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation>Vista previa</translation>
+    </message>
+    <message>
+        <source>Filename</source>
+        <translation>Nombre de fichero</translation>
+    </message>
+    <message>
+        <source>MIME type</source>
+        <translation>Tipo MIME</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Tamaño</translation>
+    </message>
+    <message>
+        <source>There is no image file.</source>
+        <translation>No hay ninguna imagen.</translation>
+    </message>
+    <message>
+        <source>Remove image</source>
+        <translation>Eliminar imagen</translation>
+    </message>
+    <message>
+        <source>New image file for upload</source>
+        <translation>Nueva imagen para subir</translation>
+    </message>
+    <message>
+        <source>Alternative image text</source>
+        <translation>Texto alternativo</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/ezie</name>
+    <message>
+        <source>Edit</source>
+        <translation>Editar</translation>
+    </message>
+    <message>
+        <source>eZ Image Editor</source>
+        <translation>eZ Image Editor</translation>
+    </message>
+    <message>
+        <source>Save and Close</source>
+        <translation>Guardar y cerrar</translation>
+    </message>
+    <message>
+        <source>Save &amp;amp; Close</source>
+        <translation>Guardar &amp;amp; Cerrar</translation>
+    </message>
+    <message>
+        <source>Close without saving</source>
+        <translation>Cerrar sin guardar</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Salir</translation>
+    </message>
+    <message>
+        <source>Thumbnail</source>
+        <translation>Miniatura</translation>
+    </message>
+    <message>
+        <source>Rotation</source>
+        <translation>Rotación</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Seleccionar</translation>
+    </message>
+    <message>
+        <source>Dimensions</source>
+        <translation>Dimensiones</translation>
+    </message>
+    <message>
+        <source>Keep ratio</source>
+        <translation>Mantener proporciones</translation>
+    </message>
+    <message>
+        <source>Free</source>
+        <translation>Libre</translation>
+    </message>
+    <message>
+        <source>Crop</source>
+        <translation>Recortar</translation>
+    </message>
+    <message>
+        <source>Zoom</source>
+        <translation>Zoom</translation>
+    </message>
+    <message>
+        <source>Actual pixels</source>
+        <translation>Tamaño original</translation>
+    </message>
+    <message>
+        <source>Fit on screen</source>
+        <translation>Encajar en pantalla</translation>
+    </message>
+    <message>
+        <source>Watermarks</source>
+        <translation>Marcas de agua</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation>Aplicar</translation>
+    </message>
+    <message>
+        <source>Contrast</source>
+        <translation>Contraste</translation>
+    </message>
+    <message>
+        <source>Brightness</source>
+        <translation>Brillo</translation>
+    </message>
+    <message>
+        <source>Actions</source>
+        <translation>Acciones</translation>
+    </message>
+    <message>
+        <source>Tools</source>
+        <translation>Herramientas</translation>
+    </message>
+    <message>
+        <source>Undo</source>
+        <translation>Deshacer</translation>
+    </message>
+    <message>
+        <source>Redo</source>
+        <translation>Rehacer</translation>
+    </message>
+    <message>
+        <source>Image</source>
+        <translation>Imagen</translation>
+    </message>
+    <message>
+        <source>Horizontal Flip</source>
+        <translation>Voltear horizontalmente</translation>
+    </message>
+    <message>
+        <source>Vertical Flip</source>
+        <translation>Voltear verticalmente</translation>
+    </message>
+    <message>
+        <source>Perform Crop</source>
+        <translation>Aplicar recorte</translation>
+    </message>
+    <message>
+        <source>Watermark</source>
+        <translation>Marca de agua</translation>
+    </message>
+    <message>
+        <source>Effects</source>
+        <translation>Efectos</translation>
+    </message>
+    <message>
+        <source>Pixelate</source>
+        <translation>Pixelar</translation>
+    </message>
+    <message>
+        <source>Black and White</source>
+        <translation>Blanco y Negro</translation>
+    </message>
+    <message>
+        <source>Sepia</source>
+        <translation>Sepia</translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>Opciones</translation>
+    </message>
+    <message>
+        <source>The image is not loaded because of a server error.</source>
+        <translation>La imagen no se ha cargado debido a un problema del servidor.</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Cerrar</translation>
+    </message>
+</context>
+</TS>
