ed8017cd357c4ae8a5481edf70df2cd304b2bf33
Damien Pobel <damien.pobel__AT__smile.fr>
2011-03-09 01:19:09 +0800

    Localization fre-FR : complete localization in fre-FR

--- translations/fre-FR/translation.ts
+++ translations/fre-FR/translation.ts
@@ -1,163 +1,198 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0" language="fr_FR">
-    <context>
-        <name>design/standard/ezie</name>
-
-        <message>
-            <source>Edit</source>
-            <translation>Éditer</translation>
-        </message>
-
-        <message>
-            <source>Save &amp; Close</source>
-            <translation>Sauvegarder et quitter</translation>
-        </message>
-
-        <message>
-            <source>Save and Close</source>
-            <translation>Sauvegarder et quitter</translation>
-        </message>
-
-        <message>
-            <source>Quit</source>
-            <translation>Quitter</translation>
-        </message>
-
-        <message>
-            <source>Close without saving</source>
-            <translation>Quitter sans sauvegarder</translation>
-        </message>
-
-        <message>
-            <source>Thumbnail</source>
-            <translation>Miniature</translation>
-        </message>
-
-        <message>
-            <source>Ok</source>
-            <translation>Ok</translation>
-        </message>
-
-        <message>
-            <source>Zoom</source>
-            <translation>Zoom</translation>
-        </message>
-
-        <message>
-            <source>Actual pixels</source>
-            <translation>Taille réelle</translation>
-        </message>
-
-        <message>
-            <source>Fit on screen</source>
-            <translation>Taille de l'écran</translation>
-        </message>
-
-
-        <message>
-            <source>Watermark</source>
-            <translation>Watermark</translation>
-        </message>
-
-        <message>
-            <source>Watermarks</source>
-            <translation>Watermarks</translation>
-        </message>
-
-        <message>
-            <source>Apply</source>
-            <translation>Appliquer</translation>
-        </message>
-
-        <message>
-            <source>Contrast</source>
-            <translation>Contraste</translation>
-        </message>
-
-        <message>
-            <source>Brightness</source>
-            <translation>Luminosité</translation>
-        </message>
-
-        <message>
-            <source>Actions</source>
-            <translation>Actions</translation>
-        </message>
-
-        <message>
-            <source>Tools</source>
-            <translation>Outils</translation>
-        </message>
-
-        <message>
-            <source>Select</source>
-            <translation>Selectionner</translation>
-        </message>
-
-        <message>
-            <source>Undo</source>
-            <translation>Annuler</translation>
-        </message>
-
-        <message>
-            <source>Redo</source>
-            <translation>Rétablir</translation>
-        </message>
-
-        <message>
-            <source>Image</source>
-            <translation>Image</translation>
-        </message>
-
-        <message>
-            <source>Horizontal Flip</source>
-            <translation>Symétrie horizontale</translation>
-        </message>
-
-        <message>
-            <source>Vertical Flip</source>
-            <translation>Symétrie verticale</translation>
-        </message>
-
-        <message>
-            <source>Crop</source>
-            <translation>Rogner</translation>
-        </message>
-
-        <message>
-            <source>Perform Crop</source>
-            <translation>Appliquer rognage</translation>
-        </message>
-
-        <message>
-            <source>Rotation</source>
-            <translation>Rotation</translation>
-        </message>
-
-        <message>
-            <source>Effects</source>
-            <translation>Effets</translation>
-        </message>
-
-        <message>
-            <source>Pixelate</source>
-            <translation>Pixelisation</translation>
-        </message>
-
-        <message>
-            <source>Black and White</source>
-            <translation>Noir et Blanc</translation>
-        </message>
-
-        <message>
-            <source>Sepia</source>
-            <translation>Sépia</translation>
-        </message>
-
-        <message>
-            <source>Options</source>
-            <translation>Options</translation>
-        </message>
-    </context>
+<context>
+    <name>design/standard/content/datatype</name>
+    <message>
+        <source>Current image</source>
+        <translation>Image courante</translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation>Prévisualisation</translation>
+    </message>
+    <message>
+        <source>Filename</source>
+        <translation>Nom du fichier</translation>
+    </message>
+    <message>
+        <source>MIME type</source>
+        <translation>Type MIME</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Taille</translation>
+    </message>
+    <message>
+        <source>There is no image file.</source>
+        <translation>Aucun fichier image</translation>
+    </message>
+    <message>
+        <source>Remove image</source>
+        <translation>Supprimer l'image</translation>
+    </message>
+    <message>
+        <source>New image file for upload</source>
+        <translation>Nouvelle image à télécharger</translation>
+    </message>
+    <message>
+        <source>Alternative image text</source>
+        <translation>Texte alternatif de l'image</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/ezie</name>
+    <message>
+        <source>Edit</source>
+        <translation>Modifier</translation>
+    </message>
+    <message>
+        <source>Save &amp; Close</source>
+        <translation type="obsolete">Sauvegarder et quitter</translation>
+    </message>
+    <message>
+        <source>Save and Close</source>
+        <translation>Enregister et quitter</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <source>Close without saving</source>
+        <translation>Quitter sans enregistrer</translation>
+    </message>
+    <message>
+        <source>Thumbnail</source>
+        <translation>Miniature</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+    <message>
+        <source>Zoom</source>
+        <translation>Zoom</translation>
+    </message>
+    <message>
+        <source>Actual pixels</source>
+        <translation>Taille réelle</translation>
+    </message>
+    <message>
+        <source>Fit on screen</source>
+        <translation>Ajuster à l&apos;écran</translation>
+    </message>
+    <message>
+        <source>Watermark</source>
+        <translation>Marquage</translation>
+    </message>
+    <message>
+        <source>Watermarks</source>
+        <translation>Marquages</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation>Appliquer</translation>
+    </message>
+    <message>
+        <source>Contrast</source>
+        <translation>Contraste</translation>
+    </message>
+    <message>
+        <source>Brightness</source>
+        <translation>Luminosité</translation>
+    </message>
+    <message>
+        <source>Actions</source>
+        <translation>Actions</translation>
+    </message>
+    <message>
+        <source>Tools</source>
+        <translation>Outils</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Sélectionner</translation>
+    </message>
+    <message>
+        <source>Undo</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <source>Redo</source>
+        <translation>Rétablir</translation>
+    </message>
+    <message>
+        <source>Image</source>
+        <translation>Image</translation>
+    </message>
+    <message>
+        <source>Horizontal Flip</source>
+        <translation>Symétrie horizontale</translation>
+    </message>
+    <message>
+        <source>Vertical Flip</source>
+        <translation>Symétrie verticale</translation>
+    </message>
+    <message>
+        <source>Crop</source>
+        <translation>Rogner</translation>
+    </message>
+    <message>
+        <source>Perform Crop</source>
+        <translation>Appliquer le rognage</translation>
+    </message>
+    <message>
+        <source>Rotation</source>
+        <translation>Rotation</translation>
+    </message>
+    <message>
+        <source>Effects</source>
+        <translation>Effets</translation>
+    </message>
+    <message>
+        <source>Pixelate</source>
+        <translation>Pixelisation</translation>
+    </message>
+    <message>
+        <source>Black and White</source>
+        <translation>Noir et blanc</translation>
+    </message>
+    <message>
+        <source>Sepia</source>
+        <translation>Sépia</translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>Options</translation>
+    </message>
+    <message>
+        <source>eZ Image Editor</source>
+        <translation>eZ Image Editor</translation>
+    </message>
+    <message>
+        <source>Save &amp;amp; Close</source>
+        <translation>Enregister &amp;amp; quitter</translation>
+    </message>
+    <message>
+        <source>Dimensions</source>
+        <translation>Dimensions</translation>
+    </message>
+    <message>
+        <source>Keep ratio</source>
+        <translation>Conserver l'aspect</translation>
+    </message>
+    <message>
+        <source>Free</source>
+        <translation>Libre</translation>
+    </message>
+    <message>
+        <source>The image is not loaded because of a server error.</source>
+        <translation>L'image n'est pas chargée en raison d'une erreur côté serveur.</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Quitter</translation>
+    </message>
+</context>
 </TS>
