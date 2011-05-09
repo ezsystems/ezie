c70e94def748c30e86ea66a403e46b0327abb1c6
xc <xc@ez.no>
2011-05-09 15:03:14 +0200

    Fixed #018275: German translation for eZ Image Editor

---
+++ translations/ger-DE/translation.ts
@@ -0,0 +1,186 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>design/standard/content/datatype</name>
+    <message>
+        <source>Current image</source>
+        <translation>Aktuelles Bild</translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation>Vorschau</translation>
+    </message>
+    <message>
+        <source>Filename</source>
+        <translation>Dateiname</translation>
+    </message>
+    <message>
+        <source>MIME type</source>
+        <translation>MIME type</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Größe</translation>
+    </message>
+    <message>
+        <source>There is no image file.</source>
+        <translation>Es gibt keine Bilddatei.</translation>
+    </message>
+    <message>
+        <source>Remove image</source>
+        <translation>Bild entfernen</translation>
+    </message>
+    <message>
+        <source>New image file for upload</source>
+        <translation>Neues Bild für upload</translation>
+    </message>
+    <message>
+        <source>Alternative image text</source>
+        <translation>Alternativer Bildtext</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/ezie</name>
+    <message>
+        <source>Edit</source>
+        <translation>Bearbeiten</translation>
+    </message>
+    <message>
+        <source>eZ Image Editor</source>
+        <translation>eZ Image Editor</translation>
+    </message>
+    <message>
+        <source>Save and Close</source>
+        <translation>Speichern und Beenden</translation>
+    </message>
+    <message>
+        <source>Save &amp;amp; Close</source>
+        <translation>Speichern &amp;amp; Beenden</translation>
+    </message>
+    <message>
+        <source>Close without saving</source>
+        <translation>Beenden ohne Speichern</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Beenden</translation>
+    </message>
+    <message>
+        <source>Thumbnail</source>
+        <translation>Miniaturansicht</translation>
+    </message>
+    <message>
+        <source>Rotation</source>
+        <translation>Rotation</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Auswählen</translation>
+    </message>
+    <message>
+        <source>Dimensions</source>
+        <translation>Abmessungen</translation>
+    </message>
+    <message>
+        <source>Keep ratio</source>
+        <translation>Seitenverhältnis</translation>
+    </message>
+    <message>
+        <source>Free</source>
+        <translation>Frei</translation>
+    </message>
+    <message>
+        <source>Crop</source>
+        <translation>Zuschneiden</translation>
+    </message>
+    <message>
+        <source>Zoom</source>
+        <translation>Zoom</translation>
+    </message>
+    <message>
+        <source>Actual pixels</source>
+        <translation>Aktuelle pixel</translation>
+    </message>
+    <message>
+        <source>Fit on screen</source>
+        <translation>Bildschirmgröße</translation>
+    </message>
+    <message>
+        <source>Watermarks</source>
+        <translation>Wasserzeichen</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation>Anwenden</translation>
+    </message>
+    <message>
+        <source>Contrast</source>
+        <translation>Kontrast</translation>
+    </message>
+    <message>
+        <source>Brightness</source>
+        <translation>Helligkeit</translation>
+    </message>
+    <message>
+        <source>Actions</source>
+        <translation>Aktionen</translation>
+    </message>
+    <message>
+        <source>Tools</source>
+        <translation>Werkzeuge</translation>
+    </message>
+    <message>
+        <source>Undo</source>
+        <translation>Rückgängig</translation>
+    </message>
+    <message>
+        <source>Redo</source>
+        <translation>Wiederholen</translation>
+    </message>
+    <message>
+        <source>Image</source>
+        <translation>Bild</translation>
+    </message>
+    <message>
+        <source>Horizontal Flip</source>
+        <translation>Horizontal spiegeln</translation>
+    </message>
+    <message>
+        <source>Vertical Flip</source>
+        <translation>Vertikal spiegeln</translation>
+    </message>
+    <message>
+        <source>Perform Crop</source>
+        <translation>Zuschneiden</translation>
+    </message>
+    <message>
+        <source>Watermark</source>
+        <translation>Wasserzeichen</translation>
+    </message>
+    <message>
+        <source>Effects</source>
+        <translation>Effekte</translation>
+    </message>
+    <message>
+        <source>Pixelate</source>
+        <translation>Verpixeln</translation>
+    </message>
+    <message>
+        <source>Black and White</source>
+        <translation>Schwarz/Weiß</translation>
+    </message>
+    <message>
+        <source>Sepia</source>
+        <translation>Sepia</translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>Optionen</translation>
+    </message>
+</context>
+</TS>
