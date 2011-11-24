cd65a087b601411dba910c6207d88c82149c2209
karol <karol_trolek@o2.pl>
2011-11-24 22:14:25 +0100

    localization: added pol-PL translation.ts

---
+++ translations/pol-PL/translation.ts
@@ -0,0 +1,194 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>design/standard/content/datatype</name>
+    <message>
+        <source>Current image</source>
+        <translation>Obecny plik graficzny</translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation>Podgląd</translation>
+    </message>
+    <message>
+        <source>Filename</source>
+        <translation>Nazwa pliku</translation>
+    </message>
+    <message>
+        <source>MIME type</source>
+        <translation>Typ MIME</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Rozmiar</translation>
+    </message>
+    <message>
+        <source>There is no image file.</source>
+        <translation>Brak pliku graficznego.</translation>
+    </message>
+    <message>
+        <source>Remove image</source>
+        <translation>Usuń obraz</translation>
+    </message>
+    <message>
+        <source>New image file for upload</source>
+        <translation>Nowy plik graficzny do przesłania</translation>
+    </message>
+    <message>
+        <source>Alternative image text</source>
+        <translation>Alternatywny tekst obrazka</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/ezie</name>
+    <message>
+        <source>Edit</source>
+        <translation>Edytuj</translation>
+    </message>
+    <message>
+        <source>eZ Image Editor</source>
+        <translation>Edytor eZ Image</translation>
+    </message>
+    <message>
+        <source>Save and Close</source>
+        <translation>Zapisz i Zamknij</translation>
+    </message>
+    <message>
+        <source>Save &amp;amp; Close</source>
+        <translation>Zapisz &amp;amp; Zamknij</translation>
+    </message>
+    <message>
+        <source>Close without saving</source>
+        <translation>Zamknij bez zapisywania</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Zamknij</translation>
+    </message>
+    <message>
+        <source>Thumbnail</source>
+        <translation>Miniatura</translation>
+    </message>
+    <message>
+        <source>Rotation</source>
+        <translation>Obrót</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Zaznacz</translation>
+    </message>
+    <message>
+        <source>Dimensions</source>
+        <translation>Wymiary</translation>
+    </message>
+    <message>
+        <source>Keep ratio</source>
+        <translation>Zachowaj proporcje</translation>
+    </message>
+    <message>
+        <source>Free</source>
+        <translation>Dowolny</translation>
+    </message>
+    <message>
+        <source>Crop</source>
+        <translation>Przytnij</translation>
+    </message>
+    <message>
+        <source>Zoom</source>
+        <translation>Powiększenie</translation>
+    </message>
+    <message>
+        <source>Actual pixels</source>
+        <translation>Rzeczywisty rozmiar</translation>
+    </message>
+    <message>
+        <source>Fit on screen</source>
+        <translation>Dopasuj do ekranu</translation>
+    </message>
+    <message>
+        <source>Watermarks</source>
+        <translation>Znaki wodne</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation>Zatwierdź</translation>
+    </message>
+    <message>
+        <source>Contrast</source>
+        <translation>Kontrast</translation>
+    </message>
+    <message>
+        <source>Brightness</source>
+        <translation>Jasność</translation>
+    </message>
+    <message>
+        <source>Actions</source>
+        <translation>Akcje</translation>
+    </message>
+    <message>
+        <source>Tools</source>
+        <translation>Narzędzia</translation>
+    </message>
+    <message>
+        <source>Undo</source>
+        <translation>Cofnij</translation>
+    </message>
+    <message>
+        <source>Redo</source>
+        <translation>Ponów</translation>
+    </message>
+    <message>
+        <source>Image</source>
+        <translation>Obraz</translation>
+    </message>
+    <message>
+        <source>Horizontal Flip</source>
+        <translation>Odbij w poziomie</translation>
+    </message>
+    <message>
+        <source>Vertical Flip</source>
+        <translation>Odbij w pionie</translation>
+    </message>
+    <message>
+        <source>Perform Crop</source>
+        <translation>Przytnij</translation>
+    </message>
+    <message>
+        <source>Watermark</source>
+        <translation>Znak wodny</translation>
+    </message>
+    <message>
+        <source>Effects</source>
+        <translation>Efekty</translation>
+    </message>
+    <message>
+        <source>Pixelate</source>
+        <translation>Pikselizacja</translation>
+    </message>
+    <message>
+        <source>Black and White</source>
+        <translation>Czarno-biały</translation>
+    </message>
+    <message>
+        <source>Sepia</source>
+        <translation>Sepia</translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>Opcje</translation>
+    </message>
+    <message>
+        <source>The image is not loaded because of a server error.</source>
+        <translation>Obraz nie został wczytany, ponieważ wystąpił błąd na serwerze.</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Zamknij</translation>
+    </message>
+</context>
+</TS>
