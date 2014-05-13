c106d0b73b75752f6ef84cf08bd6bbea96cb81b8
André R <ar@ez.no>
2014-05-13 23:22:48 +0200

    Fix EZP-22128: Change license & copyright info to be in root of project

--- classes/image_analyzer.php
+++ classes/image_analyzer.php
@@ -3,8 +3,8 @@
  * File containing the eZIEImageAnalyzer class
  * This class overrides ezcImageAnalyzer in order to support the ezpublish cluster constraints
  *
- * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
- * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
  * @version //autogentag//
  * @package ezie
  */
--- modules/ezie/prepare.php
+++ modules/ezie/prepare.php
@@ -3,8 +3,8 @@
  * File containing the ezie/prepare view
  * This view prepares an image for edition, and returns its information as JSON
  *
- * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
- * @license http://ez.no/licenses/gnu_gpl GNU GPL v2
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
  * @version //autogentag//
  * @package ezie
  */
